4dcdaff20da4265bd85045bc813790bcbd562b57
Simon Patrick <99285225+SimonPatrickReyes@users.noreply.github.com>
2023-10-29 15:28:24 +0100

    Add files via upload

--- cliente/21-PROYECTO-LocalStorage-Simon/js/app.js
+++ cliente/21-PROYECTO-LocalStorage-Simon/js/app.js
@@ -2,36 +2,54 @@
 const mensaje = document.querySelector("#mensaje");
 const listaMensajes = document.querySelector("#lista-mensajes");
 const agregar = document.querySelector("input[type='submit']");
+const eliminarBtn = document.querySelector("#lista-mensajes");
 
-const mensajes = [];
-const mensajesStorage  = localStorage.getItem("mensajes")
-const mensajesJSON = JSON.parse(mensajesStorage)
+let mensajes;
+
+//Comprobamos si el localStorage esta vacio
+if (localStorage.getItem("mensajes") == null) {
+  mensajes = []
+} else {
+  mensajes = JSON.parse(localStorage.getItem("mensajes"))
+};
+
+
 
 //Listeners
 agregar.addEventListener("click", guardarMensaje);
 agregar.addEventListener("click", mostrarMensaje);
+eliminarBtn.addEventListener("click", eliminarMensaje);
 
 //Funciones
 function guardarMensaje(e) {
   e.preventDefault();
-  mensajes.push(mensaje.value);
-  localStorage.setItem("mensajes",JSON.stringify(mensajes))
-  mostrarMensaje()
+
+  const mensajeObj = {
+    id: Date.now(),
+    text: mensaje.value
+  };
+  mensajes = [...mensajes, mensajeObj]
+  
+  //Guardado en localStorage
+  mensajesToString = JSON.stringify(mensajes)
+  localStorage.setItem("mensajes", mensajesToString)
+  console.log(mensajes)
+
+  mostrarMensaje();
+  mensaje.value = "";
 }
-
-
-console.log(mensajesJSON)
 
 function mostrarMensaje() {
   limpiarHTML();
   mensajes.forEach((mensaje) => {
-    const mensajeHTML = document.createElement("p");
+    console.log(mensaje);
+    const mensajeHTML = document.createElement("tr");
     mensajeHTML.innerHTML = `
-        <p>${mensaje}</p>
+    <td><p>${mensaje.text}</p></td>
+    <td><a href="#" class="borrar-mensaje">X</a></td>
         `;
     listaMensajes.appendChild(mensajeHTML);
   });
-  
 }
 
 function limpiarHTML() {
@@ -39,3 +57,27 @@ function limpiarHTML() {
     listaMensajes.firstChild.remove();
   }
 }
+
+function eliminarMensaje(e) {
+  if (e.target.classList.contains("borrar-mensaje")) {
+
+    const row = e.target.parentElement.parentElement;
+
+    const messageText = row.querySelector('td p').textContent;
+
+    mensajes = localStorage.getItem("mensajes")
+    mensajesParse = JSON.parse(mensajes)
+
+    mensajes = mensajesParse.filter((mensaje) => mensaje.text !== messageText);
+
+    mensajesToString = JSON.stringify(mensajes)
+
+    localStorage.setItem("mensajes", mensajesToString)
+
+    mostrarMensaje();
+    console.log(mensajes)
+
+  }
+}
+
+mostrarMensaje()
